da62ac5aa4cd87593f0ad6c6cf0a22eb2c4895bb
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-02-05 22:08:21 +0100

    data: adds moses to the downloaded formats

--- data/get_corpora.sh
+++ data/get_corpora.sh
@@ -5,13 +5,13 @@ baseurl='https://wolfpit.net/share/archive/opus/'
 locales='cs,da,de,en,ja'
 corpora='ECB/v1,EMEA/v3,News-Commentary/v11,Europarl/v7,OpenSubtitles/v2018,Ubuntu/v14.10'
 
-full_url="${baseurl}{${corpora}}/tmx/{${locales}}-{${locales}}.tmx.gz"
+full_url="${baseurl}{${corpora}}/{tmx,moses}/{${locales}}-{${locales}}.{tmx.gz,txt.zip}"
 
 # Download all textcorpora
 curl 			\
 	--location	\
 	--create-dir	\
-	--output '#1/#2-#3.tmx.gz' "$full_url"
+	--output '#1/#2/#3-#4.#5' "$full_url"
 
 # Filter out all empty files
 find . -name '*.tmx.gz' -size -2k -exec rm '{}' ';'
