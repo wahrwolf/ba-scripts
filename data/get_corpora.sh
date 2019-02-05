2a1581422c3fa31b2f461ad29d7268dddea0949d
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-02-05 19:53:35 +0100

    data: uses wolfpit as new mirror

--- data/get_corpora.sh
+++ data/get_corpora.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-baseurl='http://opus.nlpl.eu/download.php'
+#baseurl='http://opus.nlpl.eu/download.php?f='
+baseurl='https://wolfpit.net/share/archive/opus/'
 
 locales='cs,da,de,en,ja'
-corpora='EMEA/v3,News-Commentary/v11,Europarl/v7,OpenSubtitles/v2018,Ubuntu/v14.10'
+corpora='ECB/v1,EMEA/v3,News-Commentary/v11,Europarl/v7,OpenSubtitles/v2018,Ubuntu/v14.10'
 
-full_url="${baseurl}?f={${corpora}}/tmx/{${locales}}-{${locales}}.tmx.gz"
+full_url="${baseurl}{${corpora}}/tmx/{${locales}}-{${locales}}.tmx.gz"
 
 # Download all textcorpora
 curl 			\
