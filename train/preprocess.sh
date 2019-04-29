1f1083b9ac2f380af44d52aed51c8995f1cb642b
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-04-29 16:12:43 +0200

    train/preprocess: adds lots of debug messages

--- train/preprocess.sh
+++ train/preprocess.sh
@@ -2,12 +2,14 @@ set -o errexit
 script_dir="${SCRIPT_DIR:-$(dirname $0)/../}"
 train_dir="${TRAIN_DIR:-$script_dir/train/}"
 
-echo "Loading $train_dir..."
-
 # shellcheck source=./util.sh
+echo -n "Loading utils from $train_dir..." 
 source "$train_dir/util.sh"
+echo "Done\!"
 
+echo -n "Loading env..." 
 load_env "$train_dir/config/environ"
+echo "Done\!"
 
 corpus_name="${1:-$CORPUS_NAME}"
 activate_debug
@@ -17,11 +19,15 @@ data_dir="${DATA_DIR:-/data/4dahmen/}"
 config_dir="${CONFIG_DIR:-${data_dir}/config}"
 corpus_dir="${CORPUS_DIR:-$data_dir/$corpus_name/}"
 
+echo -n "Loading corpus specifc env..." 
 load_env "$config_dir/$corpus_name/environ"
+echo "Done\!"
 
 target_dir="${TARGET_DIR:-${corpus_dir}/preprocess/}"
 mkdir --parent "$target_dir"
 
 onmt_dir=${ONMT_DIR:-$tmp_dir/onmt/}
 
+echo -n "Running preprocess..."
 $pipenv_bin run python $onmt_dir/preprocess.py  --config "$corpus_dir/preprocess.config"
+echo "All set!"
