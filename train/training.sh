968be26c0548a70cc9eb5ff13f5de63669392de8
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-04-30 10:11:36 +0200

    train/train: frist draft

---
+++ train/training.sh
@@ -0,0 +1,35 @@
+set -o errexit
+script_dir="${SCRIPT_DIR:-$(dirname $0)/../}"
+train_dir="${TRAIN_DIR:-$script_dir/train/}"
+
+# shellcheck source=./util.sh
+echo -n "Loading utils from $train_dir..." 
+source "$train_dir/util.sh"
+echo "Done!"
+
+load_env "$train_dir/config/environ"
+
+corpus_name="${1:-$CORPUS_NAME}"
+activate_debug
+
+tmp_dir="${TMP_DIR:-$(mktemp --directory)}"
+work_dir="${WORK_DIR:-$tmp_dir/workbench}"
+data_dir="${DATA_DIR:-/data/4dahmen/}"
+config_dir="${CONFIG_DIR:-${data_dir}/config}"
+corpus_dir="${CORPUS_DIR:-$data_dir/$corpus_name/}"
+
+load_env "$config_dir/$corpus_name/environ"
+
+target_dir="${TARGET_DIR:-${corpus_dir}/preprocess/}"
+mkdir --parent "$target_dir"
+
+pip_dir="${PIP_DIR:-$(dirname $script_dir)}"
+pipenv_bin="${pip_dir}/bin/pipenv"
+onmt_dir=${ONMT_DIR:-$tmp_dir/onmt/}
+
+echo -n "Running train..."
+mkdir --parent "$work_dir"
+cd "$work_dir"
+export PYTHONPATH=$PYTHONPATH:$pip_dir/lib/$(ls $pip_dir/lib)/site-packages/
+$pipenv_bin run python $onmt_dir/train.py  --config "$corpus_dir/train.config"
+echo "All set!"
