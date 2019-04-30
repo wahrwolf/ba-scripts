6d953a87bc6815660917b321ca84f865e3316d80
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-05-01 00:44:08 +0200

    config/mqtt: move worker config to new dir

--- train/mqtt-worker.sh
+++ train/mqtt-worker.sh
@@ -5,16 +5,16 @@ set -o errexit
 source "$(dirname $0)/util.sh"
 activate_debug
 
-tmp_dir="${TMP_DIR:-$(mktemp --directory)}"
-work_dir="${WORK_DIR:-$tmp_dir/workbench}"
-data_dir="${DATA_DIR:-/data/4dahmen/}"
-config_dir="${CONFIG_DIR:-${data_dir}/config}"
-corpus_dir="${CORPUS_DIR:-$data_dir/$corpus_name/}"
+export tmp_dir="${TMP_DIR:-$(mktemp --directory)}"
+export work_dir="${WORK_DIR:-$tmp_dir/workbench}"
+export data_dir="${DATA_DIR:-/data/4dahmen/}"
+export config_dir="${CONFIG_DIR:-${data_dir}/config}"
+export corpus_dir="${CORPUS_DIR:-$data_dir/$corpus_name/}"
 
-script_dir=${SCRIPT_DIR:-$tmp_dir/scripts/}
-onmt_dir=${ONMT_DIR:-$tmp_dir/onmt/}
-bish_dir=${BISH_DIR:-$tmp_dir/bish/}
+export script_dir=${SCRIPT_DIR:-$tmp_dir/scripts/}
+export onmt_dir=${ONMT_DIR:-$tmp_dir/onmt/}
+export bish_dir=${BISH_DIR:-$tmp_dir/bish/}
 
 mkdir --parent "$work_dir"
 cd "$work_dir"
-$bish_dir/bish-bosh --verbose 1 -- "$config_dir/mqtt-worker.snippet"
+$bish_dir/bish-bosh --verbose 1 -- "$config_dir/mqtt/gpu-worker.snippet"
