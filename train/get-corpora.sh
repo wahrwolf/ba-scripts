90a11cc8219d06b77b516c97ac181d31dfec34ee
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-05-08 09:18:20 +0200

    get-corpora: try to pack the env again at one place

--- train/get-corpora.sh
+++ train/get-corpora.sh
@@ -6,16 +6,14 @@ export DEBUG_MAIL_TAG="[BA][$corpus_name]"
 # shellcheck source=./util.sh
 source "$(dirname $0)/util.sh"
 activate_debug
+load_runtime $CONFIG_DIR
+
+#expose_runtime
+export target_dir="${data_dir}/${corpus_name}"
+export target_config="${config_dir}/$corpus_name"/$(basename --suffix=.template "${config_template}")
+export tmp_file="${tmp_dir}/${corpus_name}.tar.gz"
 
 corpus_host="${CORPUS_HOST:-https://wolfpit.net/share/archive/corpora/}"
-
-export tmp_dir="${TMP_DIR:-$(mktemp --directory)}"
-export data_dir="${DATA_DIR:-/data/4dahmen/}"
-export config_dir="${CONFIG_DIR:-${data_dir}/config}"
-export template_dir="${TEMPLATE_DIR:-$script_dir/config}"
-
-export target_dir="${data_dir}/${corpus_name}"
-export tmp_file="${tmp_dir}/${corpus_name}.tar.gz"
 
 echo "${corpus_host}/${corpus_name}.tar.gz --[${tmp_dir}]--> ${target_dir}"
 curl "${corpus_host}/${corpus_name}.tar.gz" --out "${tmp_file}"
@@ -25,7 +23,6 @@ tar --extract --gzip --file "${tmp_file}" --one-top-level="${target_dir}"
 load_env "$config_dir/$corpus_name/environ"
 for config_template in "${template_dir}"/*.template;
 do	
-	export target_config="${config_dir}/$corpus_name"/$(basename --suffix=.template "${config_template}")
 	if [ -f "$config_dir/$corpus_name/$(basename "$target_config")" ]
 	then
 		# use corpora specif config if available
