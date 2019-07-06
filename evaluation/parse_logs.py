da561149cf667a685765041aa81129c624090869
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-07-06 02:49:19 +0200

    eval/parser: can now work on dirs and haz fancy bar

--- evaluation/parse_logs.py
+++ evaluation/parse_logs.py
@@ -3,12 +3,12 @@ This script offers some methods to parse logging files.
 Thanks to Fire this can also be used directly
 """
 
-from os import environ
-from os.path import isfile
+from os import environ, listdir
+from os.path import isfile, isdir
 import re
-from dateutil.parser import parse
 from logging import info, warning, debug, basicConfig
 from fire import Fire
+from tqdm import tqdm
 
 RULES = {
         "train": {
@@ -16,7 +16,7 @@ RULES = {
             "valid acc" : re.compile("^.+ Validation accuracy: (?P<valid_accuracy>.+)"),
             "valid ppl" : re.compile("^.+ Validation perplexity: (?P<valid_perplexity>.+)")
         }, "config": {
-            "run"               : re.compile("Testing .+/(?P<run>train.[^/]+)/(?P<model>(?P<corpus>[^/]+)_step_(?P<step>\d+).pt)"),
+            "run"               : re.compile("Testing .+/(?P<run>train.[^/]+)/(?P<model>(?P<corpus>[^/]+)_step_.+.pt)"),
             "start_time"        : re.compile(r"^\[(?P<start_time>.+) INFO\] Starting training .*"),
             "end_time"          : re.compile(r"^\[(?P<end_time>.+) INFO\] Saving checkpoint .*"),
             "model_path_train"  : re.compile("^save_model: ?\"(?P<model_path>.+)\""),
@@ -25,20 +25,26 @@ RULES = {
             "start_decay_steps" : re.compile("start_decay_steps: (?P<start_decay_steps>.+)"),
             "corpus_train"      : re.compile("Using config from .+/(?P<corpus>[^/]+)/.+?config$"),
             "type"              : re.compile("Using config from .+/(?P<type>[^/]+).+?config$")
+        }, "preprocess": {
+        }, "translate": {
         }, "score": {
-            "bleu": re.compile("^BLEU = (?P<BLEU>.+?),.+$")
+            "bleu": re.compile("^BLEU = (?P<BLEU>.+?),.+$"),
+            "step": re.compile("Testing .+/(?P<run>train.[^/]+)/(?P<model>[^/]+_step_(?P<step>\d+).pt)"),
             }
         }
 
 def extract_config(rules, path):
     with open(path) as log_file:
         config = {}
+        n_lines = 0
         for line in log_file:
+            n_lines += 1
             # apply all regex to each line
             for rule, regex in rules["config"].items():
                 if regex.match(line):
                     matches = regex.match(line).groupdict()
                     config.update(matches)
+    config["length"] = n_lines
     return config
 
 def extract_train_stats(rules, config, path):
@@ -47,9 +53,8 @@ def extract_train_stats(rules, config, path):
     """
     model = []
     with open(path) as log_file:
-        defaults = ["step"]
-        current_step = {k: config.get(k) for k in defaults if k in config}
-        for line in log_file:
+        current_step = {}
+        for line in tqdm(log_file, total=config["length"]):
             # apply regex to each line
             for rule, regex in rules[config["type"]].items():
                 if regex.match(line):
@@ -64,30 +69,47 @@ def extract_train_stats(rules, config, path):
                     break
     return model
 
-def parse(log_files):
+def parse(target_dirs):
     """
     Parse log files and return everything as a dict
     """
-    if isinstance(log_files, list):
-        debug(f"Going over {len(log_files)} file...")
+    log_files = []
+    if isinstance(target_dirs, list):
+        debug(f"Going over {len(target_dirs)} directories...")
     else:
-        debug(f"Running in single file mode!")
-        log_files = [log_files]
+        debug(f"Running in single mode!")
+        target_dirs = [target_dirs]
+
+    for directory in target_dirs:
+        if isdir(directory):
+            debug(f"Checking {directory}")
+            for log_file in listdir(directory):
+                debug(f"-found {log_file}")
+                log_files.append(f"{directory}/{log_file}")
+        elif isfile(directory):
+            debug(f"Target {directory} seems to be a file... Adding it directly")
+            log_files.append(directory)
+
 
     models = {}
-    for log in log_files:
+    for log in tqdm(log_files):
         if isfile(log):
             debug(f"Found log '{log}'")
             # pass over file and try to get the config section
             config = extract_config(RULES, log)
             debug("Extracted the following config:")
             debug(config)
+            if not all(k in config for k in ["type", "corpus"]):
+                continue
+
             corpus = config.get("corpus")
             if corpus not in models:
-                models[corpus] = []
-            models[corpus].append({"params": config, "steps" : extract_train_stats(RULES, config, log)})
+                models[corpus] = {}
+            if config["type"] not in models[corpus]:
+                models[corpus][config["type"]] = []
+            models[corpus][config["type"]].append({"params": config, "steps" : extract_train_stats(RULES, config, log)})
         else:
-            warning(f"File '{log}' not found!")
+            warning(f"File '{log}' not accesible!")
     return models
 
 if __name__ == '__main__':
