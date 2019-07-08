cdea5eb6c60f190bbd7c985d730f4c24328aae1c
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-07-08 12:47:57 +0200

    train/train: use queue instead simple files

--- train/train.sh
+++ train/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
-script_dir="${SCRIPT_DIR:-$(dirname $0)/../}"
+script_dir="${SCRIPT_DIR:-$(dirname "$0")/../}"
 train_dir="${TRAIN_DIR:-$script_dir/train/}"
 corpus_name="${1:-$CORPUS_NAME}"
 
@@ -18,22 +18,26 @@ mkdir --parent "$target_dir"
 
 load_env "$config_dir/$corpus_name/environ"
 
-echo "Running train..."
-if [ ! -f   "$config_dir/$corpus_name/train.config" ]
-then
-	echo "Config not found!"
-	exit 1
-elif [ -n "$(ls "$target_dir")" ]
-then
-	echo "Target dir not empty!"
-	exit 1
-else
-echo "Using config from $config_dir/$corpus_name/train.config"
-cat  "$config_dir/$corpus_name/train.config"
-fi
+for config in "$config_dir/$corpus_name/queue/"*.
+do
+	echo "Running train..."
+	if [ ! -f   "$config_dir/$corpus_name/train.config" ]
+	then
+		echo "Config not found! Loading next from queue"
+		cp "$config_dir/$corpus_name"/{queue/"$config",train.config} 
+		
+	elif [ -n "$(ls "$target_dir")" ]
+	then
+		echo "Target dir not empty!"
+		exit 1
+	else
+	echo "Using config from $config_dir/$corpus_name/train.config"
+	cat  "$config_dir/$corpus_name/train.config"
+	fi
 
-cd "$work_dir"
-$pipenv_bin run python "$onmt_dir/train.py"  --config "$config_dir/$corpus_name/train.config"
-echo -n "Finished training! Backup files..."
-mv "$target_dir" "$corpus_dir/train.$TIME"
-echo "Done"
+	cd "$work_dir"
+	$pipenv_bin run python "$onmt_dir/train.py"  --config "$config_dir/$corpus_name/train.config"
+	echo -n "Finished training! Backup files..."
+	mv "$target_dir" "$config_dir/$corpus_name/train.config" "$corpus_dir/train.$TIME"
+	echo "Done"
+done
