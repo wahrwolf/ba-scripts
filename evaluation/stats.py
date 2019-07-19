3742d31ccdf3ae3d6c3c06ace9ef485e7fadaa4b
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-07-19 12:04:47 +0200

    eval/stats: use true value comparison

--- evaluation/stats.py
+++ evaluation/stats.py
@@ -12,12 +12,12 @@ def calculate_perfect_validation(log, corpus, run):
     for step, entry in enumerate(log[corpus]["train"][run]["steps"]):
         max_valid = (
                     float(entry.get("step", 0)),
-                    float(entry.get("valid_accuracy",0))
-                ) if float(entry.get("valid_accuracy",0)) > max_valid[1] else max_valid
+                    float(entry.get("valid_accuracy", 0))
+                ) if float(entry.get("valid_accuracy", 0)) > max_valid[1] else max_valid
         max_train = (
                     float(entry.get("step", 0)),
-                    float(entry.get("train_accuracy",0))
-                ) if float(entry.get("train_accuracy",0)) > max_train[1] else max_train
+                    float(entry.get("train_accuracy", 0))
+                ) if float(entry.get("train_accuracy", 0)) > max_train[1] else max_train
     return {"valid": max_valid, "train": max_train}
 
 if __name__ == '__main__':
