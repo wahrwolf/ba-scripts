6f614117d052ecdd2631069b88ef81cc987388b9
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-07-26 14:53:44 +0200

    eval/parse: changes rule structure

--- evaluation/parse_logs.py
+++ evaluation/parse_logs.py
@@ -4,6 +4,7 @@ Thanks to Fire this can also be used directly
 """
 
 from os import environ, listdir
+from sqlite3 import connect
 from os.path import isfile, isdir
 import re
 from logging import info, warning, debug, basicConfig
@@ -19,42 +20,56 @@ DEFAULT_CONFIG = {
         }
 
 RULES = {
-        "train": {
-            "step"      : re.compile("^.+ Step (?P<step>\d+)/.+ acc: (?P<train_accuracy>.+); ppl: (?P<train_perplexity>.+?);.+"),
-            "valid acc" : re.compile("^.+ Validation accuracy: (?P<valid_accuracy>.+)"),
-            "valid ppl" : re.compile("^.+ Validation perplexity: (?P<valid_perplexity>.+)")
-        }, "config": {
-            "run"               : re.compile("Testing .+/(?P<run>train.[^/]+)/(?P<model>(?P<corpus>[^/]+)_step_.+.pt)"),
-            "start_time"        : re.compile(r"^\[(?P<start_time>.+) INFO\] Starting training .*"),
-            "end_time"          : re.compile(r"^\[(?P<end_time>.+) INFO\] Saving checkpoint .*"),
-            "model_path_train"  : re.compile("^save_model: ?\"(?P<model_path>[^\"']+)\""),
-            "optim"             : re.compile("optim: ?[\"']?(?P<optim>[^\"']+?)[\"']?$"),
-            "learning_rate"     : re.compile("learning_rate: ?[\"']?(?P<learning_rate>[^\"']+?)[\"']?$"),
-            "start_decay_steps" : re.compile("start_decay_steps: ?[\"']?(?P<start_decay_steps>[^\"']+?)[\"']?$"),
-            "corpus_train"      : re.compile("Using config from .+/(?P<corpus>[^/]+)/.+?config$"),
+        "meta": {
             "type"              : re.compile("Using config from .+/(?P<type>[^/]+).+?config$")
-        }, "preprocess": {
-        }, "translate": {
-        }, "score": {
-            "bleu": re.compile("^BLEU = (?P<BLEU>.+?),.+$"),
-            "step": re.compile("Testing .+/(?P<run>train.[^/]+)/(?P<model>[^/]+_step_(?P<step>\d+).pt)"),
+            , "train" : {
+                "start_time"        : re.compile(r"^\[(?P<start_time>.+) INFO\] Starting training .*"),
+                "end_time"          : re.compile(r"^\[(?P<end_time>.+) INFO\] Saving checkpoint .*"),
+                "optim"             : re.compile("optim: ?[\"']?(?P<optim>[^\"']+?)[\"']?$"),
+                "learning_rate"     : re.compile("learning_rate: ?[\"']?(?P<learning_rate>[^\"']+?)[\"']?$"),
+                "start_decay_steps" : re.compile("start_decay_steps: ?[\"']?(?P<start_decay_steps>[^\"']+?)[\"']?$"),
+                "corpus_train"      : re.compile("Using config from .+/(?P<corpus>[^/]+)/.+?config$"),
+            }, "score" : {
+                "start_time"        : re.compile(r"^\[(?P<start_time>.+) INFO\] Translating shard 0."),
+                "duration"          : re.compile(r"^Total translation time \(s\): (?P<duration> .+)$"),
+                "run"               : re.compile("Testing .+/(?P<run>train.[^/]+)/(?P<model>(?P<corpus>[^/]+)_step_.+.pt)"),
+            }, "translate" :{
+            }, "preprocess" :{
+            }
+        }, "body": {
+            "train" : {
+                "step"      : re.compile("^.+ Step (?P<step>\d+)/.+ acc: (?P<train_accuracy>.+); ppl: (?P<train_perplexity>.+?);.+"),
+                "valid acc" : re.compile("^.+ Validation accuracy: (?P<valid_accuracy>.+)"),
+                "valid ppl" : re.compile("^.+ Validation perplexity: (?P<valid_perplexity>.+)")
+            }, "score" : {
+                "bleu": re.compile("^BLEU = (?P<BLEU>.+?),.+$"),
+            }, "translate" :{
+            }, "preprocess" :{
             }
         }
+    }
 
 def extract_config(rules, path):
     with open(path) as log_file:
         config = {"path" : path}
         n_lines = 0
         for line in log_file:
+            if rules["meta"]["type"].match(line):
+                action_type = rules["meta"]["type"].match(line).groupdict()["type"]
+                log_file.seek(0)
+                config["type"] = action_type
+                break
+
+        for line in log_file:
             n_lines += 1
             config["length"] = n_lines
-            for rule, regex in rules["config"].items():
+            for rule, regex in rules["meta"][action_type].items():
                 n_lines += 1
                 # apply all regex to each line
                 if regex.match(line):
                     matches = regex.match(line).groupdict()
                     config.update(matches)
-    return {**DEFAULT_CONFIG.get(config.get("type", "train"),{}), **config}
+    return {**DEFAULT_CONFIG.get(config.get("type", "train"), {}), **config}
   #  return config
 
 def extract_train_stats(rules, config, path):
@@ -66,7 +81,7 @@ def extract_train_stats(rules, config, path):
         current_step = {}
         for line in tqdm(log_file, disable=(__name__ != '__main__'), total=config["length"]):
             # apply regex to each line
-            for rule, regex in rules[config["type"]].items():
+            for rule, regex in rules["body"][config["type"]].items():
                 if regex.match(line):
                     debug(f"Rule {rule} matched")
                     matches = regex.match(line).groupdict()
@@ -110,6 +125,7 @@ def parse(target_dirs):
             debug("Extracted the following config:")
             debug(config)
             if not all(k in config for k in ["type", "corpus"]):
+                debug("Missing some config values! Skipping...")
                 continue
 
             corpus = config.get("corpus")
@@ -121,6 +137,7 @@ def parse(target_dirs):
         else:
             warning(f"File '{log}' not accesible!")
     return models
+
 
 if __name__ == '__main__':
     if "LOGGING" in environ:
