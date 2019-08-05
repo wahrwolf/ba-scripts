7592e35b16ac35716755dcf38803d59676090f0d
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-08-05 08:48:22 +0200

    score: fixed another small typo.. I should not code so late)

--- train/score.sh
+++ train/score.sh
@@ -104,7 +104,7 @@ function do_job {
 		echo "[$run]: " $("$onmt_dir/tools/multi-bleu-detok.perl" \
 			-lc "$run/reference.txt" \
 			< "$run/translation-$(basename --suffix .pt "$model").txt" \
-			>> "$run/bleu-$(basename --suffix .pt "$model".score)")
+			>> "$run/bleu-$(basename --suffix .pt "$model").score")
 		echo "[$run]: Calculating ROUGE:"
 		echo "[$run]: " $($pipenv_bin run python \
 				-m rouge.rouge \
