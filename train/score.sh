a0aa9f6676b9ef2689881a0a1ef7058ade9c6345
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-08-05 02:37:58 +0200

    train/scores: fixes darn subshell redirecting

--- train/score.sh
+++ train/score.sh
@@ -99,12 +99,12 @@ function do_job {
 		echo "[$run]: Calculating BLEU:"
 		echo "[$run]: " $("$onmt_dir/tools/multi-bleu-detok.perl" \
 		   	"$run/reference.txt" \
-			< "$run/translation-$(basename --suffix .pt "$model").txt") \
-			> "run/bleu-$(basename --suffix .pt "$model").score"
+			< "$run/translation-$(basename --suffix .pt "$model").txt" \
+			> "run/bleu-$(basename --suffix .pt "$model").score")
 		echo "[$run]: " $("$onmt_dir/tools/multi-bleu-detok.perl" \
 			-lc "$run/reference.txt" \
-			< "$run/translation-$(basename --suffix .pt "$model").txt") \
-			>> "run/bleu-$(basename --suffix .pt "$model").score"
+			< "$run/translation-$(basename --suffix .pt "$model").txt" \
+			>> "run/bleu-$(basename --suffix .pt "$model".score)")
 		echo "[$run]: Calculating ROUGE:"
 		echo "[$run]: " $($pipenv_bin run python \
 				-m rouge.rouge \
